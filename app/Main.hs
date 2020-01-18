15f060281395e46e8d7257ec8625a141f5d7dc76
mrsekut <k.cloudspider@gmail.com>
2020-01-18 12:01:32 +0900

    [Add]env by Map

--- app/Main.hs
+++ app/Main.hs
@@ -4,21 +4,33 @@ import           Lexer.Lexer
 import           Parser.Parser
 import           Parser.AST
 import           Data.IORef
+import qualified Data.Map                      as M
 
 
 
-type Env = IORef [(String, IORef Exp)]
+type Env = M.Map String Exp
 
-emptyEnv :: IO Env
-emptyEnv = newIORef []
+emptyEnv :: Env
+emptyEnv = M.empty
+
+envBind :: String -> Exp -> Env -> Env
+envBind s v env = M.insert s v env
+
+envLookup :: String -> Env -> Exp
+envLookup x env = case M.lookup x env of
+    Just exp -> exp
+    Nothing  -> (Int (-1)) -- error
+
 
 
 eval :: Exp -> Env -> Int
-eval (Int n    ) _   = n
-eval (Plus  a b) env = (eval a env) + (eval b env)
-eval (Minus a b) env = (eval a env) - (eval b env)
-eval (Times a b) env = (eval a env) * (eval b env)
-eval (Div   a b) env = (eval a env) `quot` (eval b env)
+eval (Int n     ) _   = n
+eval (Plus   a b) env = (eval a env) + (eval b env)
+eval (Minus  a b) env = (eval a env) - (eval b env)
+eval (Times  a b) env = (eval a env) * (eval b env)
+eval (Div    a b) env = (eval a env) `quot` (eval b env)
+eval (Assign n x) env = eval x (envBind n x env)
+eval (Var x     ) env = eval (envLookup x env) env
 
 
 main :: IO ()
@@ -26,3 +38,9 @@ main = do
     let f   = "x = 2"
     let ast = parse $ lexer f
     print $ "ast: " ++ show ast
+    print $ "eval: " ++ show (eval ast emptyEnv)
+
+    let f   = "x + 2"
+    let ast = parse $ lexer f
+    print $ "ast: " ++ show ast
+    print $ "eval: " ++ show (eval ast emptyEnv)
